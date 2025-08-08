0bc4cbfe4443efec74fd657a352e90720376af88
ayush <ayush2bhoyar@gmail.com>
2025-08-08 19:13:09 -0400

    Modified the index.js

---
+++ new_index.js
@@ -0,0 +1,11 @@
+// Simple Node.js script for testing Git reset
+
+function greet(name) {
+    console.log(`Hello, ${name}!`);
+}
+
+greet("World");
+
+dhddjdjdd
+
+// Change this function or add more lines to test different commits
